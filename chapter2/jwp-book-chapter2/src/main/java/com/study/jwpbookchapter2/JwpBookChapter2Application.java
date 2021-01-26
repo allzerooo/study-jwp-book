839d9a2059fb8650c38cb642d263b790962aca76
최다영 <dychoi.dev@gmail.com>
2021-01-26 23:54:10 +0900

    추가: 실습 프로젝트

---
+++ chapter2/jwp-book-chapter2/src/main/java/com/study/jwpbookchapter2/JwpBookChapter2Application.java
@@ -0,0 +1,13 @@
+package com.study.jwpbookchapter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpBookChapter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpBookChapter2Application.class, args);
+	}
+
+}
---
+++ chapter2/jwp-book-chapter2/src/test/java/com/study/jwpbookchapter2/JwpBookChapter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jwpbookchapter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwpBookChapter2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
