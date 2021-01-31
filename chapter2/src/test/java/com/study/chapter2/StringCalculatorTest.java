712590c6ef2978f869a81059c83d95ddc332dca7
최다영 <dychoi.dev@gmail.com>
2021-01-31 14:22:25 +0900

    테스트: 커스텀 구분자 찾기 추가

--- chapter2/src/test/java/com/study/chapter2/StringCalculatorTest.java
+++ chapter2/src/test/java/com/study/chapter2/StringCalculatorTest.java
@@ -3,6 +3,9 @@ package com.study.chapter2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
@@ -14,5 +17,22 @@ class StringCalculatorTest {
         String[] nums = str.split("[:,]");
 
         assertEquals(3, nums.length);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 찾기")
+    void findCustomSeparator() {
+        String str = "//;₩n1;2;3";
+
+        Pattern pattern = Pattern.compile("[/][/](.*?)[₩][n]");
+        Matcher matcher = pattern.matcher(str);
+
+        String separator = null;
+
+        while (matcher.find()) {
+            separator = matcher.group(1);
+        }
+
+        assertEquals(";", separator);
     }
 }
