654b8fb92b8cb2016dce14bbaf692690f010c934
최다영 <dychoi.dev@gmail.com>
2021-02-01 22:54:21 +0900

    책 따라해보기 - 2. add() 메서드 추가하기

---
+++ chapter2/src/main/java/com/study/chapter2/StringCalculator.java
@@ -0,0 +1,14 @@
+package com.study.chapter2;
+
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+    public int add(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
