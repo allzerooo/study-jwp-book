0bb5dd6cc4a4cee34f07b8ff8ee508bb28eafd88
최다영 <dychoi@docfriends.com>
2021-02-15 23:33:29 +0900

    문자열 계산기 구현 및 리팩토링 1단계 - 요구사항 1번(빈 문자열 또는 NULL 값을 입력할 경우 0을 반환해야 한다) 테스트 추가

--- chapter2/src/test/java/com/study/chapter2/StringCalculatorTest.java
+++ chapter2/src/test/java/com/study/chapter2/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.study.chapter2;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,4 +11,17 @@ import java.util.regex.Pattern;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setup() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    void add_null_또는_빈문자열() {
+        assertEquals(0, stringCalculator.add(null));
+        assertEquals(0, stringCalculator.add(""));
+    }
 }
