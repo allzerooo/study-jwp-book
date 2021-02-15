31259cdbb2e38ed12e1c69b36afee3b0e8f25ef0
최다영 <dychoi@docfriends.com>
2021-02-15 22:37:17 +0900

    문자열 계산기 구현 및 리팩토링 1단계 - 요구사항 1번(빈 문자열 또는 NULL 값을 입력할 경우 0을 반환해야 한다) 구현 추가

--- chapter2/src/main/java/com/study/chapter2/StringCalculator.java
+++ chapter2/src/main/java/com/study/chapter2/StringCalculator.java
@@ -5,6 +5,10 @@ import java.util.regex.Pattern;
 public class StringCalculator {
 
     public int add(String text) {
+        // 요구사항 1. 빈 문자열 또는 NULL 값을 입력할 경우 0을 반환해야 한다
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
         return 0;
     }
 }
